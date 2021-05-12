05cf1c489f0cca85658b2745f7c8bb4df37e7499
Mitch Harding <mitchell.harding@hpe.com>
2021-05-11 19:52:54 -0500

    CASMCMS-7049: Add MIT license to source files; Simplify versioning; Update README; Add copyright/license linter

--- runBuildPrep.sh
+++ runBuildPrep.sh
@@ -1,4 +1,6 @@
-# Copyright 2020-2021 Hewlett Packard Enterprise Development LP
+#!/usr/bin/env sh
+
+# Copyright 2021 Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -19,36 +21,7 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 #
 # (MIT License)
+#
 
-"""
-Obtaining information from a k8s cluster.
-"""
-
-from cfsssh.context import in_cluster
-
-
-class KubeToken(object):
-    """
-    A Kube Token is a string representing an authentication string; it is exposed
-    in various locations within the management plane.
-    """
-    @classmethod
-    def from_pod(cls):
-        """
-        A KubeToken is automatically injected into running pods; it can be obtained
-        by simply reading the associated file.
-        """
-        self = cls.__new__(cls)
-        if in_cluster():
-            with open('/var/run/secrets/kubernetes.io/serviceaccount/token', 'r') as token_file:
-                self.value = token_file.read().strip()
-        else:
-            raise ValueError("KubeToken cannot be obtained using this method")
-        return self
-
-    def __str__(self):
-        return self.value
-
-    def __repr__(self):
-        return str(self)
-
+./update_versions.sh || exit 1
+exit 0
